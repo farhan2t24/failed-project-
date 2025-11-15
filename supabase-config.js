07efe7e833f5ca80da5794c05c9b51babb02254a
Farhan Ahmed Sharieff <farhan2t24@gmail.com>
2025-11-15 22:51:34 +0530

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,8 +0,0 @@
-// Supabase Configuration
-const supabaseConfig = {
-    url: "https://gstbstgifrgjpunncokc.supabase.co",
-    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzdGJzdGdpZnJnanB1bm5jb2tjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3ODMxMTYsImV4cCI6MjA3ODM1OTExNn0.vQGrkVdYP_KZKXMrgTl-2zgrzvXFfxoq0NsiU83-e7g"
-};
-
-// Make config available globally
-window.supabaseConfig = supabaseConfig;
